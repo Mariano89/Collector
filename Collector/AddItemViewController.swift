e1a68ce24f290af314f2174e9146542f45206f58
Mariano Echegoyen <mariano.echegoyen@dng.com>
2017-12-30 19:59:32 -0800

    can pick an image from camera roll and display it

---
+++ Collector/AddItemViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AddItemViewController.swift
+//  Collector
+//
+//  Created by Mariano Echegoyen on 12/29/17.
+//  Copyright © 2017 Mariano Echegoyen. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imagePicker.delegate = self
+    }
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let chosenImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            itemImageView.image = chosenImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
